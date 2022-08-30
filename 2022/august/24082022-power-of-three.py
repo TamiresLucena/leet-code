c64a04acb672e780c050567ba112012ccb6bb997
Tamires Lucena <tamires.lucena@luizalabs.com>
2022-08-30 13:23:36 -0300

    24082022 - power of three

--- 2022/august/24082022-power-of-three.py
+++ 2022/august/24082022-power-of-three.py
@@ -1,3 +1,14 @@
+# Given an integer n, return true if it is a power of three. Otherwise, return false.
+# An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+# Input: n = 27
+# Output: true
+# Explanation: 27 = 33
+
+# Input: n = 0
+# Output: false
+# Explanation: There is no x where 3x = 0.
+
 def isPowerOfThree(n: int) -> bool:
     if n == 1: return True
     resultado = int(n / 3)
